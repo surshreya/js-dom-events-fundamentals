aaaa67fdd6b36b77485c172870d76d8a7e34c02f
surshreya <surshreya.0727@gmail.com>
2022-12-05 19:34:30 +0530

    open and close modal window action added

--- src/modal/script.js
+++ src/modal/script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+const closeModalBtn = document.querySelector(".close-modal");
+const openModalBtns = document.querySelectorAll(".show-modal");
+
+/**
+ * Open the Modal Window
+ */
+openModalBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+});
+
+/**
+ * Close the modal window
+ */
+
+closeModalBtn.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
