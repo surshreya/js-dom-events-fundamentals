0e849ce8b8ffdd756dbd32fda78e694a2e28ad89
surshreya <surshreya.0727@gmail.com>
2022-12-05 19:39:06 +0530

    closing modal on clicking outside modal window added

--- src/modal/script.js
+++ src/modal/script.js
@@ -6,21 +6,29 @@ const overlay = document.querySelector(".overlay");
 const closeModalBtn = document.querySelector(".close-modal");
 const openModalBtns = document.querySelectorAll(".show-modal");
 
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 /**
  * Open the Modal Window
  */
 openModalBtns.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+  btn.addEventListener("click", openModal);
 });
 
 /**
- * Close the modal window
+ * Close the modal window on X button
  */
+closeModalBtn.addEventListener("click", closeModal);
 
-closeModalBtn.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+/**
+ * Close the modal window when clicked outside the modal
+ */
+overlay.addEventListener("click", closeModal);
