6d90205a63493eced2201126c2d0d0f47c39a06d
surshreya <surshreya.0727@gmail.com>
2022-12-06 20:44:19 +0530

    pig game initial logic added

--- src/pigGame/script.js
+++ src/pigGame/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+/**
+ * Selecting elements
+ */
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.querySelector(".current--0");
+const current1El = document.querySelector(".current--1");
+
+const btnRoll = document.querySelector("btn--roll");
+const btnHold = document.querySelector("btn--hold");
+
+const btnNew = document.querySelector("btn--new");
+
+const diceEl = document.querySelector("dice");
